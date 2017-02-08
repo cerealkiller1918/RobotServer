5940d66ce7a4a102a563c09eadccae8b2297fcdd
Justin Frasier <Justin_Frasier@me.com>
2017-02-08 14:03:06 -0600

    working on turning it into a byte array and back

--- src/main/java/com/justinfrasier/robot/setup/webcam/WebCam.java
+++ src/main/java/com/justinfrasier/robot/setup/webcam/WebCam.java
@@ -1,30 +1,89 @@
 package com.justinfrasier.robot.setup.webcam;
 
 import org.bytedeco.javacpp.opencv_core.*;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.Frame;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
 
 import static org.bytedeco.javacpp.opencv_highgui.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvDecodeImage;
 import static org.bytedeco.javacpp.opencv_videoio.*;
+import static org.bytedeco.javacv.OpenCVFrameConverter.*;
 
 
 public class WebCam{
 
+    static ServerSocket serverSocket;
+    static Socket socket;
+
     public static void main(String[] args){
-        CvCapture capture = cam();
-        IplImage frame;
-        while(true) {
-            frame = cvQueryFrame(capture);
-            cvShowImage("Video",frame);
-            char c = (char) cvWaitKey(5);
-            if(c=='q') break;
+        try {
+            //serverSocket = new ServerSocket(444);
+            //socket = serverSocket.accept();
+            //OutputStream outputStream = socket.getOutputStream();
+
+            CvCapture capture = webCam();
+            IplImage frame;
+            IplImage frame2;
+            //cvNamedWindow("Video", CV_WINDOW_AUTOSIZE);
+            while (true) {
+                frame = cvQueryFrame(capture);
+                byte[] bytes = IplImageToByteArray(frame);
+                //outputStream.write(bytes);
+                frame2 = ByteArrayToIplImage(bytes);
+                cvShowImage("Test",frame2);
+                cvWaitKey(15);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
-    public static CvCapture cam() {
-        //CvCapture capture = cvCreateFileCapture("../../test.mp4");
+    public static CvCapture webCam() {
         CvCapture capture = cvCreateCameraCapture(CV_CAP_ANY);
-        cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT,1280);
-        cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 720);
         return capture;
     }
 
+    public static byte[] IplImageToByteArray(IplImage src){
+        BufferedImage image = IplImageToBufferedImage(src);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write()
+        return bytes;
+    }
+    public static IplImage ByteArrayToIplImage(byte[] bytes) throws IOException {
+
+        return BufferedImageToIplImage(BytesToBufferedImage(bytes));
+    }
+
+    public static BufferedImage BytesToBufferedImage(byte[] bytes) throws IOException {
+        ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
+        return ImageIO.read(stream);
+    }
+
+    public static IplImage BufferedImageToIplImage(BufferedImage bufImage){
+        ToIplImage toIplImage = new OpenCVFrameConverter.ToIplImage();
+        Java2DFrameConverter java2DFrameConverter = new Java2DFrameConverter();
+        IplImage iplImage = toIplImage.convert(java2DFrameConverter.convert(bufImage));
+        return iplImage;
+    }
+
+    public static BufferedImage IplImageToBufferedImage(IplImage src){
+        OpenCVFrameConverter.ToIplImage iplImage = new OpenCVFrameConverter.ToIplImage();
+        Java2DFrameConverter bimCoverter = new Java2DFrameConverter();
+        Frame frame = iplImage.convert(src);
+        BufferedImage image = bimCoverter.convert(frame);
+        BufferedImage result = (BufferedImage) image.getScaledInstance(image.getWidth(),image.getHeight(), Image.SCALE_DEFAULT);
+        image.flush();
+        return result;
+    }
 }
