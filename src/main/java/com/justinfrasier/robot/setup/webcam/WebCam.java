d9fdb5a5b24ef71ba678a3d87294c7f8d96a82f1
Justin Frasier <Justin_Frasier@me.com>
2017-02-09 11:37:24 -0600

    it is streaming video now

--- src/main/java/com/justinfrasier/robot/setup/webcam/WebCam.java
+++ src/main/java/com/justinfrasier/robot/setup/webcam/WebCam.java
@@ -3,14 +3,14 @@ package com.justinfrasier.robot.setup.webcam;
 import org.bytedeco.javacpp.opencv_core.*;
 import org.bytedeco.javacv.*;
 import org.bytedeco.javacv.Frame;
+import sun.awt.image.InputStreamImageSource;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
@@ -25,27 +25,48 @@ public class WebCam{
 
     static ServerSocket serverSocket;
     static Socket socket;
-
+    static BufferedImage image;
+    static byte[] bytes;
+    static DataOutputStream outputStream;
+    static InputStreamReader inputStream;
+    static byte[] inputBytes;
     public static void main(String[] args){
         try {
-            //serverSocket = new ServerSocket(444);
-            //socket = serverSocket.accept();
-            //OutputStream outputStream = socket.getOutputStream();
+            serverSocket = new ServerSocket(444);
+            socket = serverSocket.accept();
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            inputStream = new InputStreamReader(socket.getInputStream());
 
-            CvCapture capture = webCam();
-            IplImage frame;
-            IplImage frame2;
-            //cvNamedWindow("Video", CV_WINDOW_AUTOSIZE);
-            while (true) {
-                frame = cvQueryFrame(capture);
-                byte[] bytes = IplImageToByteArray(frame);
-                //outputStream.write(bytes);
-                frame2 = ByteArrayToIplImage(bytes);
-                cvShowImage("Test",frame2);
-                cvWaitKey(15);
-            }
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    CvCapture capture = webCam();
+                    IplImage frame;
+                    while(true) {
+                        try {
+                            //Thread.sleep(500);
+                            frame = cvQueryFrame(capture);
+                            bytes = IplImageToByteArray(frame);
+                            outputStream.writeInt(bytes.length);
+                            outputStream.write(bytes);
+                            outputStream.flush();
+                            //if(getStringFromInputStreamReader(inputStream) == "good")
+                            image = BytesToBufferedImage(bytes);
+
+
+                        }catch (Exception e){
+
+                        }
+                    }
+                }
+
+
+            };
+            thread.start();
+            //new Window();
+            System.out.print("Hello");
         }catch (Exception e){
-            e.printStackTrace();
+           main(null);
         }
     }
 
@@ -54,14 +75,16 @@ public class WebCam{
         return capture;
     }
 
-    public static byte[] IplImageToByteArray(IplImage src){
+    public static byte[] IplImageToByteArray(IplImage src) throws IOException {
         BufferedImage image = IplImageToBufferedImage(src);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ImageIO.write()
-        return bytes;
+        ImageIO.write(image,"jpg",outputStream);
+        outputStream.flush();
+        byte[] imageBytes = outputStream.toByteArray();
+        outputStream.close();
+        return imageBytes;
     }
     public static IplImage ByteArrayToIplImage(byte[] bytes) throws IOException {
-
         return BufferedImageToIplImage(BytesToBufferedImage(bytes));
     }
 
@@ -79,11 +102,49 @@ public class WebCam{
 
     public static BufferedImage IplImageToBufferedImage(IplImage src){
         OpenCVFrameConverter.ToIplImage iplImage = new OpenCVFrameConverter.ToIplImage();
-        Java2DFrameConverter bimCoverter = new Java2DFrameConverter();
+        Java2DFrameConverter bimConverter = new Java2DFrameConverter();
         Frame frame = iplImage.convert(src);
-        BufferedImage image = bimCoverter.convert(frame);
-        BufferedImage result = (BufferedImage) image.getScaledInstance(image.getWidth(),image.getHeight(), Image.SCALE_DEFAULT);
-        image.flush();
-        return result;
+        BufferedImage image = bimConverter.convert(frame);
+        //return FlipImage(image);
+        return image;
+    }
+
+    public static BufferedImage FlipImage(BufferedImage src){
+        AffineTransform affineTransform = AffineTransform.getScaleInstance(-1.0, 1.0);
+        affineTransform.translate(-src.getWidth(),0);
+        AffineTransformOp transformOp = new AffineTransformOp(affineTransform,null);
+        return transformOp.filter(src,null);
+    }
+
+    public static byte[] getBytesFromInputStream(InputStream is) throws IOException
+    {
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream())
+        {
+            byte[] buffer = new byte[0xFFFF];
+
+            for (int len; (len = is.read(buffer)) != -1;)
+                os.write(buffer, 0, len);
+
+            os.flush();
+
+            return os.toByteArray();
+        }
+    }
+
+    public static BufferedImage getBufferImage(){
+        return image;
+    }
+
+    public static byte[] getBytesArray(){
+        return bytes;
+    }
+
+    public static String getStringFromInputStreamReader(InputStreamReader is) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        int i;
+        while ((i=is.read())!=-1){
+            builder.append((char) i);
+        }
+        return builder.toString();
     }
 }
