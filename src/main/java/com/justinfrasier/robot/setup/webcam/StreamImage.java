373111dbbe22e47665499e66c5f23ef94b082cfd
Justin Frasier <Justin_Frasier@me.com>
2017-02-10 22:52:47 -0600

    Refactored  some more move the runnable to a method

--- src/main/java/com/justinfrasier/robot/setup/webcam/StreamImage.java
+++ src/main/java/com/justinfrasier/robot/setup/webcam/StreamImage.java
@@ -4,51 +4,65 @@ package com.justinfrasier.robot.setup.webcam;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_videoio;
 
+import java.io.IOException;
+
 import static org.bytedeco.javacpp.opencv_videoio.CV_CAP_ANY;
 import static org.bytedeco.javacpp.opencv_videoio.cvCreateCameraCapture;
 import static org.bytedeco.javacpp.opencv_videoio.cvQueryFrame;
 
-public class StreamImage {
+class StreamImage {
 
     private Transaction transaction;
     private SyncList list;
     private ImageConverter converter;
     private byte[] bytes;
 
-    public StreamImage(Transaction transaction) {
+    StreamImage(Transaction transaction) {
         this.transaction = transaction;
         converter = new ImageConverter();
     }
 
-    public void startLoop(){
+    void startLoop(){
         list = transaction.getList();
-        Runnable runnable = () ->{
-            opencv_videoio.CvCapture capture = webCam();
-            opencv_core.IplImage frame;
-            while(true) {
-                try {
-                    frame = cvQueryFrame(capture);
-                    bytes = converter.IplImageToByteArray(frame);
-                    for(int i = 0; i < list.listSize(); i++) {
-                       try {
-                           list.getOutputStream(i).writeInt(bytes.length);
-                           list.getOutputStream(i).write(bytes);
-                           list.getOutputStream(i).flush();
-                       }catch (Exception e){
-                           list.removeOutputStream(i);
-                           System.out.println("Client has left");
-                       }
-                       Thread.sleep(10);
-                    }
-                }catch (Exception e){}
-            }
-        };
-        Thread thread = new Thread(runnable);
+        Thread thread = new Thread(getRunnable());
         thread.start();
     }
 
-    public opencv_videoio.CvCapture webCam() {
-        opencv_videoio.CvCapture capture = cvCreateCameraCapture(CV_CAP_ANY);
-        return capture;
+    private Runnable getRunnable() {
+        return () ->{
+                opencv_videoio.CvCapture capture = webCam();
+                opencv_core.IplImage frame;
+            while (true) try {
+                frame = cvQueryFrame(capture);
+                bytes = converter.IplImageToByteArray(frame);
+                iterateThroughClients();
+            } catch (Exception ignored) {}
+            };
+    }
+
+    private void iterateThroughClients() throws InterruptedException {
+        for (int i = 0; i < list.listSize(); i++) {
+            try {
+                pushImage(i);
+            } catch (Exception e) {
+                removeClient(i);
+            }
+            Thread.sleep(10);
+        }
+    }
+
+    private void removeClient(int i) {
+        list.removeOutputStream(i);
+        System.out.println("Client has left");
+    }
+
+    private void pushImage(int i) throws IOException {
+        list.getOutputStream(i).writeInt(bytes.length);
+        list.getOutputStream(i).write(bytes);
+        list.getOutputStream(i).flush();
+    }
+
+    private opencv_videoio.CvCapture webCam() {
+        return cvCreateCameraCapture(CV_CAP_ANY);
     }
 }
