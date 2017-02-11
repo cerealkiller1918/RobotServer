db34d3491eac9faac3a053f2d4b95fa79c558ae5
Justin Frasier <Justin_Frasier@me.com>
2017-02-10 22:42:12 -0600

    Refactored . Moved some code into methods and removed some code that is not being used

--- src/main/java/com/justinfrasier/robot/setup/webcam/Master.java
+++ src/main/java/com/justinfrasier/robot/setup/webcam/Master.java
@@ -1,28 +1,13 @@
 package com.justinfrasier.robot.setup.webcam;
 
 
-import org.bytedeco.javacpp.opencv_core;
-import org.bytedeco.javacpp.opencv_videoio;
-
-import java.awt.image.BufferedImage;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import static org.bytedeco.javacpp.opencv_videoio.CV_CAP_ANY;
-import static org.bytedeco.javacpp.opencv_videoio.cvCreateCameraCapture;
-import static org.bytedeco.javacpp.opencv_videoio.cvQueryFrame;
-
 public class Master {
 
     private ServerSocket serverSocket;
-    private Socket socket;
-    private BufferedImage image;
-    private byte[] bytes;
-    private DataOutputStream outputStream;
-    private static ImageConverter converter = new ImageConverter();
-    private SyncList outputStreamList;
     private Transaction transaction;
     private StreamImage streamImage;
 
@@ -32,50 +17,24 @@ public class Master {
             transaction = new Transaction();
             streamImage = new StreamImage(transaction);
             streamImage.startLoop();
-            while (true){
-                System.out.println("Waiting on client");
-                transaction.add(serverSocket.accept());
-                System.out.println("Client joined");
-            }
-        }catch (IOException e){}
+            while (true) try {
+                acceptClient();
+            } catch (IOException ignored){}
+        }catch (IOException ignored){}
     }
 
-
-
-    private void initOutputStream() throws IOException {
-        outputStream = new DataOutputStream(socket.getOutputStream());
+    private void acceptClient() throws IOException {
+        transaction.add(getAccept());
+        System.out.println("Client joined");
     }
 
-    private void SetupSocketFromServerSocket() throws IOException {
-        socket = serverSocket.accept();
+    private Socket getAccept() throws IOException {
+        return serverSocket.accept();
     }
 
     private void SetupServerSocket() throws IOException {
         serverSocket = new ServerSocket(444);
     }
 
-    private void UpdateImageLoop() {
-        opencv_videoio.CvCapture capture = webCam();
-        opencv_core.IplImage frame;
-        while(true) {
-            try {
-                frame = cvQueryFrame(capture);
-                bytes = converter.IplImageToByteArray(frame);
-                outputStream.writeInt(bytes.length);
-                outputStream.write(bytes);
-                outputStream.flush();
-                image = converter.BytesToBufferedImage(bytes);
-            }catch (Exception e){}
-        }
-    }
-
-    public opencv_videoio.CvCapture webCam() {
-        opencv_videoio.CvCapture capture = cvCreateCameraCapture(CV_CAP_ANY);
-        return capture;
-    }
-
-    public BufferedImage getBufferImage(){
-        return image;
-    }
 
 }
